2a98cdc8cf7f46aa6d47d5eba729e3f6cbdff964
LouPolish <mmluczynski@gmail.com>
2026-01-15 17:04:24 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Caches app shell for offline use. Document content is stored in IndexedDB.
  */
 
-const CACHE_NAME = 'speed-reader-v1';
+const CACHE_NAME = 'speed-reader-v2';
 const APP_SHELL = [
     '/',
     '/index.html',
