0b2386fe99a6a906558273666ead5fd360c176a4
LouPolish <mmluczynski@gmail.com>
2026-01-19 10:21:57 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Caches app shell for offline use. Document content is stored in IndexedDB.
  */
 
-const CACHE_NAME = 'red-reader-v26';
+const CACHE_NAME = 'red-reader-v27';
 const APP_SHELL = [
     '/',
     '/index.html',
